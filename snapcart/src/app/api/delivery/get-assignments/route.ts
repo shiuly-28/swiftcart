805cc0ccb9a68e9741a9c1c34e0b0d3c3bbf7b73
shiuly-28 <shulybd1245@gmail.com>
2026-04-26 22:34:08 +0600

    delivedry route implement

---
+++ snapcart/src/app/api/delivery/get-assignments/route.ts
@@ -0,0 +1,22 @@
+import { auth } from "@/auth";
+import connectDb from "@/lib/db";
+import DeliveryAssigment from "@/models/deliveryAssignment.model";
+import { NextResponse } from "next/server";
+
+export async function GET(){
+    try{
+        await connectDb()
+        const session=await auth()
+        const assignments=await DeliveryAssigment.find({
+            broadcastedTo:session?.user?.id,
+            status:"brodcasted"
+        })
+        return NextResponse.json(
+            assignments,{status:200}
+        )
+    } catch(error){
+        return NextResponse.json(
+            {message:`get assignments error $(error)`},{status:200}
+        )
+    }
+}
