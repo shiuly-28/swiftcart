a0ed10ecea4a425531e7c66027dc56725432017a
shiuly-28 <shulybd1245@gmail.com>
2026-06-25 20:12:41 +0600

    admindasboardClient file create

--- snapcart/src/components/AdminDashBoard.tsx
+++ snapcart/src/components/AdminDashBoard.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import AdminDashboardClient from './AdminDashboardClient'
 
 function AdminDashBoard() {
   return (
-    <div>
-    
-    </div>
+   <>
+   <AdminDashboardClient/>
+   </>
   )
 }
 
---
+++ snapcart/src/components/AdminDashboardClient.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AdminDashboardClient() {
+  return (
+    <div className=''>
+      
+    </div>
+  )
+}
+
+export default AdminDashboardClient
