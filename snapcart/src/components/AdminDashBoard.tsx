2d79239155c2e47d60ab85a56813a2dea308c47d
shiuly-28 <shulybd1245@gmail.com>
2026-06-27 19:24:20 +0600

    select dropdown menu  setup

--- snapcart/src/components/AdminDashBoard.tsx
+++ snapcart/src/components/AdminDashBoard.tsx
@@ -8,11 +8,34 @@ import Grocery from '@/models/grocery.model'
 async function AdminDashBoard() {
   await connectDb()
   const orders = await Order.find({})
-  const user = await User.find({role:"user"})
+  const users = await User.find({role:"user"})
   const groceries=await Grocery.find({})
+
+  const totalOrders=orders.length
+  const totalCustomers=users.length
+  const pendingDelivers=orders.filter((o)=>o.status==="pending").length
+  const totalRevenue=orders.reduce((sum,o)=>sum+(o.totalAmount || 0),0)
+
+  const today = new Date()
+  const startOfToday=new Date(today)
+  startOfToday.setHours(0, 0, 0)
+
+  const sevenDaysAgo=new Date()
+  sevenDaysAgo.setDate(today.getDate()-6)
+
+  const todayOrders=orders.filter((o) =>new Date(o.createAt)>=startOfToday)
+  const todayRevenue=todayOrders.reduce((sum,o)=>sum+(o.totalAmount || 0),0)
+
+  const sevenDaysOrders=orders.filter((o)=>new Date(o.createAt)>=sevenDaysAgo)
+  const sevenDaysRevenue=sevenDaysOrders.reduce((sum,o)=>sum+(o.totalAmount || 0),0)
   return (
    <>
-   <AdminDashboardClient/>
+   <AdminDashboardClient
+   earning={{
+    today:todayRevenue,
+     sevenDays:sevenDaysRevenue,
+      total:totalRevenue
+   }}/>
    </>
   )
 }
--- snapcart/src/components/AdminDashboardClient.tsx
+++ snapcart/src/components/AdminDashboardClient.tsx
@@ -1,8 +1,25 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import {motion} from "motion/react"
 
-function AdminDashboardClient() {
+type propType={
+  earning:{
+    today:number,
+    sevenDays:number,
+    total:number
+  }
+}
+function AdminDashboardClient({earning}:propType) {
+  const [filter, setFilter]=useState<"today" | "sevenDays" | "total">()
+
+  const currenEarning=filter==="today"?earning.today
+  :filter==="sevenDays"?earning.sevenDays
+  :earning.total
+
+  const title=filter==="today"?"Today's Earning"
+  :filter==="sevenDays"?"Last 7 Days Earning"
+  :"Total Earning"
+
   return (
     <div className='pt-28 w-[90%] mx-auto'>
       <div className='flex flex-col sm:flex-row sm:items-center justify-between gap-4 mb-10 text-center sm:text-left'>
@@ -15,12 +32,24 @@ function AdminDashboardClient() {
            🛒 Admin Dashboard
         </motion.h1>
         <select className='border border-gray-300 rounded-lg px-4 py-2 text-sm focus:ring-2
-        focus:ring-amber-500 outline-none transition w-full sm:w-auto'>
-          <option value="last 7 days">Last 7 Days</option>
-          <option value="Today">Today</option>
+        focus:ring-amber-500 outline-none transition w-full sm:w-auto'
+        onChange={(e)=>setFilter(e.target.value as any)}
+        value={filter}>
           <option value="total">Total</option>
+          <option value="sevenDays">Last 7 Days</option>
+          <option value="today">Today</option>
+          
         </select>
       </div>
+
+      <motion.div 
+       initial={{opacity: 0, y: 15}}
+        animate={{opacity: 1, y: 0}}
+        transition={{duration: 0.3}}
+        className='bg-amber-50 border border-amber-200 shadow-sm rounded-2xl p-6 text-center mb-10'>
+          <h2>{title}</h2>
+          <h2>{currenEarning.toLocaleString()}</h2>
+      </motion.div>
     </div>
   )
 }
