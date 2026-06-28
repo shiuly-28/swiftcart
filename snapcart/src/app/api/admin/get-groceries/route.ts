61a04d20848e1be163368699a239a7ba70104f35
shiuly-28 <shulybd1245@gmail.com>
2026-06-28 10:24:17 +0600

    admin route file create

--- snapcart/src/app/api/admin/get-groceries/route.ts
+++ snapcart/src/app/api/admin/get-groceries/route.ts
@@ -1,7 +1,11 @@
+import Grocery from "@/models/grocery.model";
+import { NextResponse } from "next/server";
+
 export async function GET(){
     try{
-
+        const groceries=await Grocery.find({})
+        return NextResponse.json(groceries,{status:20})
     }catch(error){
-
+        return NextResponse.json({message:`get groceris error ${error}`}, {status:200})
     }
 }
