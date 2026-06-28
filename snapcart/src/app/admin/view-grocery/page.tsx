79c231f3d2a4104ceab57e039ece86bd19c07bd6
shiuly-28 <shulybd1245@gmail.com>
2026-06-28 11:21:10 +0600

    search ber implement

--- snapcart/src/app/admin/view-grocery/page.tsx
+++ snapcart/src/app/admin/view-grocery/page.tsx
@@ -2,7 +2,7 @@
 import axios from 'axios'
 import React, { useEffect } from 'react'
 import {motion} from 'motion/react'
-import { ArrowLeft } from 'lucide-react'
+import { ArrowLeft, Package, Search } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 
 
@@ -32,8 +32,20 @@ function ViewGrocery() {
         onClick={()=>router.push("/")}
         className='flex items-center justify-center gap-2 bg-amber-100 hover:bg-amber-200 text-amber-600 font-semibold px-4 rounded-full transition w-full sm:w-auto'
         ><ArrowLeft size={18}/><span>Back</span></button>
-        <h1>Manage Groceries</h1>
+        <h1 className='text-2xl md:text-3xl font-extrabold text-amber-600 flex items-center'>
+            <Package size={30} className='text-amber-600'/>Manage Groceries</h1>
       </motion.div>
+
+      <motion.form
+       initial={{opacity:0, x: 10}} 
+      animate={{opacity:1, x: 0}}
+      transition={{duration:0.4}}
+      
+      className='flex sm:flex-row items-center bg-white border border-gray-200 rounded-full px-5 py-3 shadow-sm mb-10 hover:shadow-lg transition-all max-w-lg mx-auto w-fullgap-4'
+      >
+        <Search className='text-gray-500 w-5 h-5 mr-2'/>
+        <input type='text' className='w-full outline-none text-gray-700 placeholder-gray-400'  placeholder='Search by name or category...' />
+      </motion.form>
     </div>
   )
 }
