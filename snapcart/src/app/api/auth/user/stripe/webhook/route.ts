14dccf08067fc523aea25d21d2d4633873cf3b33
shiuly-28 <shulybd1245@gmail.com>
2026-07-06 16:39:21 +0600

    stripe init inside function error fixed

--- snapcart/src/app/api/auth/user/stripe/webhook/route.ts
+++ snapcart/src/app/api/auth/user/stripe/webhook/route.ts
@@ -3,25 +3,28 @@ import Order from "@/models/order.model";
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe=new Stripe(process.env.STRPE_SECRET_KEY!)
-
-export async function POST(req:NextRequest){
-    const sig=req.headers.get("stripe-signature")
-    const rawBody=await req.text()
-    let event;
-    try{
-        event=stripe.webhooks.constructEvent(
-            rawBody,sig!,process.env.STRIPE_WEBOOK_SECRET!
-        )
-    } catch (error){
-        console.log("signature verification failed",error)
-    }
-    if(event?.type==="checkout.session.completed"){
-        const session=event.data.object
-        await connectDb()
-        await Order.findByIdAndUpdate(session?.metadata?.orderId,{
-            isPaid:true
-        })
-    }
-    return NextResponse.json({recieved:true},{status:200})
+export async function POST(req: NextRequest) {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+  
+  const sig = req.headers.get("stripe-signature");
+  const rawBody = await req.text();
+  let event;
+  
+  try {
+    event = stripe.webhooks.constructEvent(
+      rawBody, sig!, process.env.STRIPE_WEBOOK_SECRET!
+    );
+  } catch (error) {
+    console.log("signature verification failed", error);
+  }
+  
+  if (event?.type === "checkout.session.completed") {
+    const session = event.data.object;
+    await connectDb();
+    await Order.findByIdAndUpdate(session?.metadata?.orderId, {
+      isPaid: true
+    });
+  }
+  
+  return NextResponse.json({ recieved: true }, { status: 200 });
 }
